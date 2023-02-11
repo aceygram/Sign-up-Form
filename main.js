38b24ca7390ddb628c5bef9aae59c11357d2c894
Promise Okeke <ace4facebook@gmail.com>
2023-02-11 16:05:00 +0100

    created and added functions for password validation

---
+++ main.js
@@ -0,0 +1,15 @@
+let password = document.querySelector('#user_password');
+let confirmPass = document.querySelector('#confirm_password');
+let message = document.querySelector('.message');
+
+function validate() {
+    if (confirmPass.value != password.value) {
+    message.innerText = '* passwords do not match';
+    message.style.color = 'red';
+    confirmPass.className = 'error2';
+    } else {
+    message.innerText = '* passwords match';
+    message.style.color = 'green';
+    confirmPass.className = 'pass';
+    }
+}
